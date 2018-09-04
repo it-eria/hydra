75067c69992034a1d229f2e47cb85889bb8ab81a
Vitaliy <pyzhyck@gmail.com>
2018-09-04 19:59:42 +0300

    Working on home page

---
+++ src/js/script.js
@@ -0,0 +1,51 @@
+$(function() {
+  $('[data-js="btn--burger"]').on('click', function(e) {
+    e.preventDefault();
+    $('[data-js="header"]').toggleClass('active');
+    $('[data-js="logo"] img').toggleClass('logo__visible');
+  });
+  
+  $('[data-js="main-nav"] > ul > li').each(function(index) {
+    if($(this).find('ul').length > 0) $(this).append('<span class="arr arr--down"></span>');
+    $('[data-js="main-nav"] > ul > li > .arr--down').on('click', function() {
+      $(this).parent().toggleClass('open');
+    });
+  });
+
+  var mainSliderParams = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000
+  }
+  
+  var productsSliderParams = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    dots: true,
+    autoplay: true,
+    variableWidth: true,
+    autoplaySpeed: 5000,
+    asNavFor: '[data-js="our-products__desc-slider"]'
+  }
+
+  var productsDescSliderParams = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: false,
+    dots: false,
+    autoplay: false,
+    swipe: false,
+    draggable: false
+  }
+
+  $('[data-js="main-slider"]').slick(mainSliderParams);
+  $('[data-js="our-products__slider"]').slick(productsSliderParams);
+  $('[data-js="our-products__desc-slider"]').slick(productsDescSliderParams);
+});
